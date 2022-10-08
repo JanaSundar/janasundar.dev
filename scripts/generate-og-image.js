d83f2133799155256e578f7631372e1577bcb1bf
Janarthanan <janasundar1997@gmail.com>
2022-10-08 08:39:14 +0530

    Removed puppeteer core and added chrome-aws-lambda

--- scripts/generate-og-image.js
+++ scripts/generate-og-image.js
@@ -1,12 +1,37 @@
 const fs = require('fs');
-const puppeteer = require('puppeteer-core');
+const chromium = require('chrome-aws-lambda');
 const { getAllPosts } = require('./getPost');
 const { loadEnvConfig } = require('@next/env');
 
 loadEnvConfig(process.cwd());
 
 const isDev = process.env.NODE_ENV === 'development';
+const exePath =
+  process.platform === 'win32'
+    ? 'C:\\Program Files (x86)\\Google\\Chrome\\Application\\chrome.exe'
+    : process.platform === 'linux'
+    ? '/usr/bin/google-chrome'
+    : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome';
 const baseUrl = isDev ? 'http://localhost:3001' : 'http://og-image.janasundar.dev';
+
+const getOptions = async () => {
+  let options;
+  if (isDev) {
+    options = {
+      args: [],
+      executablePath: exePath,
+      headless: true,
+    };
+  } else {
+    options = {
+      args: chromium.args,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+    };
+  }
+
+  return options;
+};
 
 (async () => {
   const posts = await getAllPosts();
@@ -18,9 +43,10 @@ const baseUrl = isDev ? 'http://localhost:3001' : 'http://og-image.janasundar.de
     const url = `${baseUrl}?${qs.toString()}`;
     const ogImageDir = `./public/images/og`;
     const imagePath = `${ogImageDir}/${post.slug}.png`;
+    const options = await getOptions();
 
-    const browser = await puppeteer.launch({
-      headless: true,
+    const browser = await chromium.puppeteer.launch({
+      ...options,
     });
     const page = await browser.newPage();
     await page.setViewport({ width: 1200, height: 630 });
