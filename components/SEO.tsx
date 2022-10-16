dfc29c93c8d50fa126e5856bbbde9ad23dda091c
Janarthanan <janasundar1997@gmail.com>
2022-10-16 13:16:57 +0530

    og image issue fixed

--- components/SEO.tsx
+++ components/SEO.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { NextSeo } from 'next-seo';
-import type { OpenGraph } from 'next-seo/lib/types';
 import { useRouter } from 'next/router';
 
 interface SEOProps {
@@ -11,20 +10,15 @@ interface SEOProps {
 export default function SEO({ title, description, ...rest }: SEOProps) {
   const router = useRouter();
 
-  const openGraph: OpenGraph = React.useMemo(
-    () => ({
-      url: `https://janasundar.dev/api/og/?title={title}`,
-      title: `${title} - Janarthanan`,
-      description,
-    }),
-    [title, description]
-  );
-
   return (
     <NextSeo
       title={`${title} - Janarthanan`}
       canonical={`https://janasundar.dev${router.asPath}`}
-      openGraph={openGraph}
+      openGraph={{
+        url: `https://janasundar.dev/api/og/?title={title}`,
+        title: `${title} - Janarthanan`,
+        description,
+      }}
       {...rest}
     />
   );
